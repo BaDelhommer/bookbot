53877e894d3a05dc945c73d82d4b4266a912d54b
BaDelhommer <badelhommer@gmail.com>
2024-04-28 02:11:28 -0500

    Added a report

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    print(f"------Begin report of {book_path}------\n\n")
+    print(f"The book contains {num_words} words\n\n")
+    create_report(chars_dict)
+    print("\n\n------End Report------")
+    
 
 
 def get_num_words(text):
@@ -25,6 +29,19 @@ def get_chars_dict(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def create_report(chars):
+    dict_list = []
+    for key, value in chars.items():
+        dict_list.append({"char": key, "num": value})
+
+    dict_list.sort(reverse=True, key=sort_on)
+
+    for i in range(len(dict_list)):
+        print(f"The {dict_list[i]["char"]} character was used {dict_list[i]["num"]} times")
+
+def sort_on(dict):
+    return dict["num"]
 
 
 main()
